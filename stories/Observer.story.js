1ab53984221a67ef27ede8095a242997b635e9ce
Daniel Schmidt <dsc@insilico.dk>
2017-06-15 10:32:40 +0200

    Use actions addon Update dependencies

--- stories/Observer.story.js
+++ stories/Observer.story.js
@@ -1,10 +1,11 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
-import { storiesOf, action } from '@storybook/react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 import Observer from '../src/index'
 import ScrollWrapper from './ScrollWrapper'
 
-const Header = props => (
+const Header = props =>
   <div
     style={{
       display: 'flex',
@@ -15,92 +16,89 @@ const Header = props => (
       textAlign: 'center',
       background: 'lightcoral',
       color: 'azure',
+      ...props.style,
     }}
   >
     <h2>{props.children}</h2>
   </div>
-)
 
 storiesOf('Intersection Observer', module)
-  .add('Child as function', () => (
+  .add('Child as function', () =>
     <ScrollWrapper>
       <Observer onChange={action('Child Observer inview')}>
         {inView => <Header>{`Header inside viewport: ${inView}`}</Header>}
       </Observer>
-    </ScrollWrapper>
-  ))
-  .add('With threshold 100%', () => (
+    </ScrollWrapper>,
+  )
+  .add('With threshold 100%', () =>
     <ScrollWrapper>
       <Observer threshold={1} onChange={action('Child Observer inview')}>
-        {inView => (
-          <Header>{`Header is fully inside the viewport: ${inView}`}</Header>
-        )}
+        {inView =>
+          <Header>{`Header is fully inside the viewport: ${inView}`}</Header>}
       </Observer>
-    </ScrollWrapper>
-  ))
-  .add('With threshold 50%', () => (
+    </ScrollWrapper>,
+  )
+  .add('With threshold 50%', () =>
     <ScrollWrapper>
       <Observer threshold={0.5} onChange={action('Child Observer inview')}>
-        {inView => (
-          <Header>{`Header is fully inside the viewport: ${inView}`}</Header>
-        )}
+        {inView =>
+          <Header>{`Header is fully inside the viewport: ${inView}`}</Header>}
       </Observer>
-    </ScrollWrapper>
-  ))
-  .add('Trigger once', () => (
+    </ScrollWrapper>,
+  )
+  .add('Trigger once', () =>
     <ScrollWrapper>
       <Observer
         threshold={1}
         triggerOnce
         onChange={action('Child Observer inview')}
       >
-        {inView => (
-          <Header>{`Header was fully inside the viewport: ${inView}`}</Header>
-        )}
+        {inView =>
+          <Header>{`Header was fully inside the viewport: ${inView}`}</Header>}
       </Observer>
-    </ScrollWrapper>
-  ))
-  .add('Multiple observers', () => (
+    </ScrollWrapper>,
+  )
+  .add('Multiple observers', () =>
     <ScrollWrapper>
       <Observer threshold={1} onChange={action('Child Observer inview')}>
-        {inView => (
-          <Header>{`Header 1 is fully inside the viewport: ${inView}`}</Header>
-        )}
+        {inView =>
+          <Header>{`Header 1 is fully inside the viewport: ${inView}`}</Header>}
       </Observer>
       <Observer threshold={1} onChange={action('Child Observer inview')}>
-        {inView => (
-          <Header>{`Header 2 is fully inside the viewport: ${inView}`}</Header>
-        )}
+        {inView =>
+          <Header>{`Header 2 is fully inside the viewport: ${inView}`}</Header>}
       </Observer>
-    </ScrollWrapper>
-  ))
-  .add('Render method', () => (
+    </ScrollWrapper>,
+  )
+  .add('Render method', () =>
     <ScrollWrapper>
       <Observer
         style={{ height: 200, position: 'relative' }}
         onChange={action('Render Observer inview')}
-        render={() => (
+        render={() =>
           <div
             style={{
               position: 'absolute',
               top: 0,
               bottom: 0,
+              left: 0,
+              right: 0,
             }}
           >
-            <Header>
-              Header is only rendered once observer is in view. Make sure that the Observer controls the height, so it does not change.
+            <Header style={{ minHeight: 0, height: '100%' }}>
+              Header is only rendered once observer is in view. Make sure that
+              the Observer controls the height, so it does not change.
             </Header>
-          </div>
-        )}
+          </div>}
       />
-    </ScrollWrapper>
-  ))
-  .add('Plain child', () => (
+    </ScrollWrapper>,
+  )
+  .add('Plain child', () =>
     <ScrollWrapper>
       <Observer onChange={action('Plain Observer inview')}>
         <Header>
           Plain children are always rendered. Use onChange to monitor state.
         </Header>
       </Observer>
-    </ScrollWrapper>
-  ))
+    </ScrollWrapper>,
+  )
