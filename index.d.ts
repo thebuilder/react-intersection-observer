cd3961565836f35974b1bd8179fedc1ff4391c6d
PrettierCI <commits@prettierci.com>
2018-03-09 20:58:31 +0000

    Sync with Prettier

--- index.d.ts
+++ index.d.ts
@@ -1,20 +1,19 @@
 declare module 'react-intersection-observer' {
-  import React = require('react');
+  import React = require('react')
 
   export interface IntersectionObserverProps {
     /** Children should be either a function or a node */
-    children?: React.ReactNode | ((inView: boolean) => React.ReactNode);
+    children?: React.ReactNode | ((inView: boolean) => React.ReactNode)
 
     /** Render prop boolean indicating inView state */
-    render?(inView: boolean): React.ReactNode;
-
+    render?(inView: boolean): React.ReactNode
 
     /**
      * The `HTMLElement` that is used as the viewport for checking visibility of
      * the target.
      * Defaults to the browser viewport if not specified or if `null`.
      */
-    root?: HTMLElement | null;
+    root?: HTMLElement | null
 
     /**
      * Unique identifier for the root element - This is used to identify the
@@ -22,39 +21,39 @@ declare module 'react-intersection-observer' {
      * If you defined a root element, without adding an `id`, it will create a new
      * instance for all components.
      */
-    rootId?: string;
+    rootId?: string
 
     /**
      * Margin around the root.
      * Can have values similar to the CSS margin property,
      * e.g. `"10px 20px 30px 40px"` (top, right, bottom, left).
      */
-    rootMargin?: string;
+    rootMargin?: string
 
     /**
      * Element tag to use for the wrapping component
      * @default `'div'`
      */
-    tag?: string;
+    tag?: string
 
     /** Number between 0 and 1 indicating the the percentage that should be
      * visible before triggering. Can also be an array of numbers, to create
      * multiple trigger points.
      * @default `0`
      */
-    threshold?: number | number[];
+    threshold?: number | number[]
 
     /**
      * Only trigger this method once
      * @default `false`
      */
-    triggerOnce?: boolean;
+    triggerOnce?: boolean
 
     /** Call this function whenever the in view state changes */
-    onChange?(inView: boolean|React.FormEvent<HTMLElement>): void;
+    onChange?(inView: boolean | React.FormEvent<HTMLElement>): void
 
     /** Get a reference to the the inner DOM node */
-    innerRef?(element?: HTMLElement): void;
+    innerRef?(element?: HTMLElement): void
   }
 
   export default class IntersectionObserver extends React.Component<
