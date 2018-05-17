826b4ee2bc2b8d95570a3e87c94bc07ae00d2821
Daniel Schmidt <dsc@charlietango.dk>
2018-05-17 20:13:51 +0200

    test: improve test coverage of index.js

--- tests/Observer.test.js
+++ tests/Observer.test.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { mount } from 'enzyme'
 import Observer from '../src/index.js'
+import intersection from '../src/intersection'
 import invariant from 'invariant'
 
 jest.mock('../src/intersection')
 jest.mock('invariant')
+
+afterEach(() => {})
 
 it('Should render <Observer />', () => {
   const callback = jest.fn()
@@ -59,6 +62,99 @@ it('Should render <Observer /> render when in view', () => {
   wrapper.setState({ inView: true })
 
   expect(wrapper).toMatchSnapshot()
+})
+
+it('Should unobserve old node', () => {
+  const wrapper = mount(<Observer>Content</Observer>)
+  const instance = wrapper.instance()
+  jest.spyOn(instance, 'observeNode')
+  const node = wrapper.getDOMNode()
+  instance.handleNode(<div />)
+  expect(intersection.unobserve).toHaveBeenCalledWith(node)
+  expect(instance.observeNode).toHaveBeenCalled()
+  expect(intersection.observe).toHaveBeenCalled()
+})
+
+it('Should ensure node exists before observering', () => {
+  const wrapper = mount(<Observer>Content</Observer>)
+  const instance = wrapper.instance()
+  intersection.observe.mockReset()
+  instance.handleNode(null)
+  expect(intersection.observe).not.toHaveBeenCalled()
+})
+
+it('Should ensure node exists before unmounting', () => {
+  const wrapper = mount(<Observer>Content</Observer>)
+  const instance = wrapper.instance()
+  instance.handleNode(null)
+
+  intersection.unobserve.mockReset()
+  instance.componentWillUnmount()
+  expect(intersection.unobserve).not.toHaveBeenCalled()
+})
+
+it('Should update state onChange', () => {
+  const wrapper = mount(<Observer>Content</Observer>)
+  wrapper.instance().handleChange(true)
+  expect(wrapper.state().inView).toBe(true)
+  wrapper.instance().handleChange(false)
+  expect(wrapper.state().inView).toBe(false)
+})
+
+it('Should recreate observer when threshold change', () => {
+  const wrapper = mount(<Observer>Content</Observer>)
+  const instance = wrapper.instance()
+  jest.spyOn(instance, 'observeNode')
+
+  // Changing threshold should cause the instance to be observed once more
+  wrapper.setProps({ threshold: 0.5 })
+  expect(intersection.unobserve).toHaveBeenCalledWith(wrapper.getDOMNode())
+  expect(instance.observeNode).toHaveBeenCalled()
+})
+
+it('Should recreate observer when root change', () => {
+  const wrapper = mount(<Observer>Content</Observer>)
+  const instance = wrapper.instance()
+  jest.spyOn(instance, 'observeNode')
+
+  // Changing threshold should cause the instance to be observed once more
+  wrapper.setProps({ root: {} })
+  expect(intersection.unobserve).toHaveBeenCalledWith(wrapper.getDOMNode())
+  expect(instance.observeNode).toHaveBeenCalled()
+})
+
+it('Should recreate observer when root change', () => {
+  const wrapper = mount(<Observer>Content</Observer>)
+  const instance = wrapper.instance()
+  jest.spyOn(instance, 'observeNode')
+
+  // Changing threshold should cause the instance to be observed once more
+  wrapper.setProps({ rootMargin: '10px' })
+  expect(intersection.unobserve).toHaveBeenCalledWith(wrapper.getDOMNode())
+  expect(instance.observeNode).toHaveBeenCalled()
+})
+
+it('Should trigger onChange callback', () => {
+  const onChange = jest.fn()
+  const wrapper = mount(<Observer onChange={onChange}>Content</Observer>)
+  wrapper.instance().handleChange(true)
+  expect(onChange).toHaveBeenLastCalledWith(true)
+  wrapper.instance().handleChange(false)
+  expect(onChange).toHaveBeenLastCalledWith(false)
+})
+
+it('Should unobserve when triggerOnce comes into view', () => {
+  const wrapper = mount(<Observer triggerOnce>Content</Observer>)
+  wrapper.setState({ inView: true })
+  const node = wrapper.getDOMNode()
+  expect(intersection.unobserve).toHaveBeenCalledWith(node)
+})
+
+it('Should unobserve when unounted', () => {
+  const wrapper = mount(<Observer>Content</Observer>)
+  const node = wrapper.getDOMNode()
+  wrapper.instance().componentWillUnmount()
+  expect(intersection.unobserve).toHaveBeenCalledWith(node)
 })
 
 it('Should throw error when not passing ref', () => {
