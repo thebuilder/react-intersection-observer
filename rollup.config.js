77ecdf796d67752527711dccdafab0d123100814
Daniel Schmidt <dsc@charlietango.dk>
2019-03-16 18:16:31 +0100

    build: replace uglify with terser

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import babel from 'rollup-plugin-babel'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import replace from 'rollup-plugin-replace'
-import { uglify } from 'rollup-plugin-uglify'
+import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
 const root = process.platform === 'win32' ? path.resolve('/') : '/'
@@ -24,46 +24,41 @@ const getBabelOptions = ({ useESModules }) => ({
 })
 
 export default [
-  pkg.module
-    ? {
-        input: './src/index.tsx',
-        output: { file: pkg.module, format: 'esm', exports: 'named' },
-        external,
-        plugins: [
-          resolve({ extensions }),
-          babel(getBabelOptions({ useESModules: true })),
-        ],
-      }
-    : null,
-  pkg.main
-    ? {
-        input: './src/index.tsx',
-        output: { file: pkg.main, format: 'cjs', exports: 'named' },
-        external,
-        plugins: [
-          resolve({ extensions }),
-          babel(getBabelOptions({ useESModules: false })),
-        ],
-      }
-    : null,
-  pkg.unpkg
-    ? {
-        input: './src/index.tsx',
-        output: {
-          file: pkg.unpkg,
-          format: 'umd',
-          name: 'ReactIntersectionObserver',
-          globals,
-          exports: 'named',
-        },
-        external: Object.keys(globals),
-        plugins: [
-          resolve({ extensions }),
-          replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
-          babel(getBabelOptions({ useESModules: true })),
-          commonjs({ include: '**/node_modules/**' }),
-          uglify(),
-        ],
-      }
-    : null,
-].filter(Boolean)
+  {
+    input: './src/index.tsx',
+    output: { file: pkg.module, format: 'esm', exports: 'named' },
+    external,
+    plugins: [
+      resolve({ extensions }),
+      babel(getBabelOptions({ useESModules: true })),
+    ],
+  },
+  {
+    input: './src/index.tsx',
+    output: { file: pkg.main, format: 'cjs', exports: 'named' },
+    external,
+    plugins: [
+      resolve({ extensions }),
+      babel(getBabelOptions({ useESModules: false })),
+    ],
+  },
+  {
+    input: './src/index.tsx',
+    output: {
+      file: pkg.unpkg,
+      format: 'umd',
+      name: 'ReactIntersectionObserver',
+      globals,
+      exports: 'named',
+      sourcemap: true,
+    },
+    external: Object.keys(globals),
+    plugins: [
+      resolve({ extensions }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
+      babel(getBabelOptions({ useESModules: true })),
+      commonjs({ include: '**/node_modules/**' }),
+      terser(),
+    ],
+  },
+]
