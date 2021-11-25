249de1aa237dfb4f6755f4d1e92ffab3ccf5ee88
Daniel Schmidt <3764345+thebuilder@users.noreply.github.com>
2021-11-25 13:26:00 +0100

    fix: undo the clear observers (#523)

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { InView } from './InView';
 export { InView } from './InView';
 export { useInView } from './useInView';
-export { observe, _observerMap } from './observe';
+export { observe } from './observe';
 
 export default InView;
 
