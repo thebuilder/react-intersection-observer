e7ac6d206a16d29fa939a0551de8d35628852670
Daniel Schmidt <dsc@charlietango.dk>
2022-06-02 22:02:26 +0200

    fix: revert the bundle changes

--- scripts/build-copy.js
+++ scripts/build-copy.js
@@ -29,8 +29,8 @@ packageFieldsToRemove.forEach((field) => {
 });
 
 // Remove 'dist' from the files inside the 'dist' dir, after we move them
-const fields = ['main', 'module', 'unpkg', 'esmodule', 'typings'];
-fields.forEach((key) => (pck[key] = pck[key].replace('dist/', '')));
+const fields = ['main', 'module', 'unpkg', 'esmodule', 'exports', 'typings'];
+fields.forEach((key) => (pck[key] = pck[key]?.replace('dist/', '')));
 
 fs.writeFileSync(
   path.resolve(distDir, 'package.json'),
