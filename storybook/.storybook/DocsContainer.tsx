575d47be03f86cd3c859efbf87f5e868ff166a27
Daniel Schmidt <dsc@charlietango.dk>
2023-03-04 00:16:15 +0100

    ci: fix broken storybook build

--- storybook/.storybook/DocsContainer.tsx
+++ storybook/.storybook/DocsContainer.tsx
@@ -1,12 +1,25 @@
-import { DocsContainer as BaseContainer } from '@storybook/addon-docs';
+import React from 'react';
+import {
+  DocsContainer as BaseContainer,
+  DocsContextProps,
+} from '@storybook/addon-docs';
 import { useDarkMode } from 'storybook-dark-mode';
 import { themes } from '@storybook/theming';
 
-export const DocsContainer = ({ children, context }) => {
-  const dark = useDarkMode();
-  console.log(dark);
+export const DocsContainer = ({
+  children,
+  context,
+}: {
+  children: React.ReactNode;
+  context: DocsContextProps;
+}) => {
+  const isDark = useDarkMode();
+
   return (
-    <BaseContainer theme={dark ? themes.dark : themes.light} context={context}>
+    <BaseContainer
+      theme={isDark ? themes.dark : themes.light}
+      context={context}
+    >
       {children}
     </BaseContainer>
   );
