55ead98e499b2d320d3e4d823f5cacfd2c3cc476
Daniel Schmidt <dsc@charlietango.dk>
2026-07-14 21:00:15 +0200

    test: add node SSR project

---
+++ src/__tests__/useInView.ssr.test.ts
@@ -0,0 +1,21 @@
+import { createElement } from "react";
+import { renderToString } from "react-dom/server";
+import { useInView } from "../useInView";
+
+test("useInView renders on the server without diagnostics", () => {
+  const consoleError = vi.spyOn(console, "error").mockImplementation(() => {});
+
+  try {
+    function CompatibilityFixture() {
+      const { ref, inView } = useInView();
+      return createElement("div", { ref }, String(inView));
+    }
+
+    expect(renderToString(createElement(CompatibilityFixture))).toBe(
+      "<div>false</div>",
+    );
+    expect(consoleError).not.toHaveBeenCalled();
+  } finally {
+    consoleError.mockRestore();
+  }
+});
